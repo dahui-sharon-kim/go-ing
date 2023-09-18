1feabfb7504d51bfbcb21f7c9a08bba85755416d
kdh0711 <kdh0711@somansa.com>
2023-09-18 17:15:29 +0900

    methods

--- my_practice/utils/practice3.go
+++ my_practice/utils/practice3.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	"math"
 	"strings"
 )
 
@@ -288,22 +289,26 @@ func Practice3_14() {
 
 // Appending to a slice
 // It is common to append new elements to a slice.
+// If the backing array of s is too small to fit all the given values,
+// a bigger array will be allocated.
+// The returned slice will point to the newly allocated array.
 
 func Practice3_15() {
-	var s []int
-	printSlice3_15(s)
+	var s []int // initialize an empty slice `s`
+	printSlice3_15(s) // len=0 cap=0 []
 
 	// append works on nil slices.
-	s = append(s, 0)
-	printSlice3_15(s)
+	s = append(s, 0) // Go may allocate a new underlying array with a capacity greater than or equal to 1
+	printSlice3_15(s) // len=1 cap=1 [0]
 
 	// The slice grows as needed.
 	s = append(s, 1)
-	printSlice3_15(s)
+	printSlice3_15(s) // len=2 cap=2 [0 1]
 
 	// We can add more than one element at a time.
+	// Go aims to minimize reallocations, it might choose to allocate an array with a larger capacity to accommodate potential future appends
 	s = append(s, 2, 3, 4)
-	printSlice3_15(s)
+	printSlice3_15(s) // len=5 cap=6 [0 1 2 3 4]
 	
 }
 
@@ -311,22 +316,185 @@ func printSlice3_15(s []int) {
 	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
 }
 
+// ** Range
+// The `range` form of the for loop iterates over a slice or map
+// When ranging over a slice, two values are returned for each iteration
+// The first is the index, and the second is a copy of the element at that index
 func Practice3_16() {
-	
+	var pow = []int{1, 2, 4, 8, 16, 32, 64, 128} // pow는 slice
+	for i, v := range pow {
+		fmt.Printf("2**%d = %d\n", i, v)
+	}
 }
 
+// You can skip the index or value by assigning to `_`.
 func Practice3_17() {
-	
+	pow := make([]int, 10)
+	for i := range pow {
+		pow[i] = 1 << uint(i) // == 2**i
+	}
+	// skipping index by assigning to _
+	for _, value := range pow {
+		fmt.Printf("%d\n", value)
+	}
+}
+
+// len=dy인 슬라이스를 리턴
+// 각 element는 dx의 슬라이스
+
+
+func Pic3_18(dx, dy int) [][]uint8 {
+	picture := make([][]uint8, dy) // 2차원 배열
+	for y := 0; y < dy; y++ {
+		row := make([]uint8, dx)
+		for x := 0; x < dx; x++ {
+			row[x] = uint8((x + y) / 2) // x*y 혹은 x^y도 추천
+		}
+		picture[y] = row
+	}
+	return picture
 }
 
 func Practice3_18() {
-	
+	// pic.Show(Pic3_18)
 }
+
+// Maps
+// Map은 키에 대응하는 값을 신속히 찾는 해시테이블을 구현한 자료구조
+// map은 reference 타입
+// A map maps keys to values.
+// The zero value of a map is `nil`. A `nil` map has no keys, nor can keys be added.
+// The `make` function returns a map of the given type, initialized and ready for use.
+
+type Vertex3_19 struct {
+	Lat, Long float64
+}
+
+// 선언 방법: map[key타입]value타입
+var m3_19 map[string]Vertex3_19
 
 func Practice3_19() {
-	
+	m3_19 = make(map[string]Vertex3_19)
+	m3_19["Bell Labs"] = Vertex3_19{
+		40.68433, -74.39967,
+	}
+	fmt.Println(m3_19["Bell Labs"])
 }
 
+// Map literals
+// Map literals are like struct literals, but the keys are required.
+
+var m3_20 = map[string]Vertex3_19 {
+	"Bell Labs": Vertex3_19{
+		40.68433, -74.39967,
+	},
+	"Google": Vertex3_19{
+		37.42202, -122.08408,
+	},
+}
 func Practice3_20() {
+	fmt.Println(m3_20)
+}
+
+
+// If the top-level type is just a type name, you can omit it from the elements of the literal
+
+var m3_21 = map[string]Vertex3_19 {
+	"Bell Labs": {40.68433, -74.39967},
+	"Google"   : {37.42202, -122.08408},
+}
+
+func Practice3_21() {
+	fmt.Println(m3_21)
+}
+
+func Practice3_22() {
+	m := make(map[string]int)
+
+	m["Answer"] = 42 // Insert or update an element in map `m`
+	fmt.Println("The value:", m["Answer"])
+
+	m["Answer"] = 48
+	fmt.Println("The value:", m["Answer"])
 	
+	m["Lucky"] = 7
+	fmt.Println("The value:", m["Lucky"])
+
+	delete(m, "Answer") // Delete an element
+	fmt.Println("The value:", m["Answer"])
+
+	v, ok := m["Answer"] // Test that a key is present with a two-value assignment
+	val, presence := m["Lucky"]
+	fmt.Println("The value:", v, "Present?", ok) 
+	fmt.Println("The value:", val, "Present?", presence)
+	// In Go, when you use a `short variable declaration`,
+	// the second variable (`ok` or `presence`) is of type boolean
+	// that indicates whether the key exists in the map
+}
+
+
+func WordCount3_23(s string) map[string]int {
+	test := make(map[string]int)
+	input := strings.Fields(s)
+	for _, v := range input {
+		test[v] += 1
+	}
+	return test
+}
+
+func Practice3_23() {
+	fmt.Println(WordCount3_23(("I ate a donut. Then I ate another donut.")))
+}
+
+// Function values
+// Functions can be passed around just like other values.
+// Function values may be used as function arguments and reture values.
+func compute(fn func(float64, float64) float64) float64 {
+	return fn(3, 4)
+}
+func Practice3_24() {
+	hypot := func(x, y float64) float64 {
+		return math.Sqrt(x*x + y*y)
+	}
+	fmt.Println(hypot(5, 12)) // 13
+	fmt.Println(compute(hypot)) // 5
+	fmt.Println(compute(math.Pow)) // 
+}
+
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+
+// Go functions may be closures.
+// A closure is a function value that references variables from outside its body.
+// The function may access and assign to the referenced variables:
+// in this sence the function is "bound" to the variables.
+func Practice3_25() {
+	pos, neg := adder(), adder()
+	for i :=0 ; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*i),
+		)
+	}
+}
+
+func fibonacci() func() int {
+	prev, cur := 0, 1
+	return func() int {
+		res := prev
+		prev, cur = cur, prev+cur
+		return res
+	}
+}
+
+func Practice3_26() {
+	f := fibonacci()
+	for i :=0; i < 10; i++ {
+		fmt.Println(f())
+	}
 }
---
+++ my_practice/utils/practice4.go
@@ -0,0 +1,198 @@
+package utils
+
+import (
+	"fmt"
+	"math"
+)
+
+// Go does not have classes. However, you can define methods on types.
+// A method is a function with a special receiver argument
+// The receiver appears in its own argument list btw the func keyword and the method name.
+// In this example, the Abs method has a receiver of type Vertex named v.
+type Vertex struct {
+	X, Y float64
+}
+
+// ** Vertex의 메소드 Abs
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+// `Abs` method can be called on values of type `Vertex`
+// This means that `Abs` is a method specifically designed to work with `Vertex` values.
+func Practice4_1() {
+	v := Vertex{3, 4}
+	fmt.Println(v.Abs())
+}
+
+// Methods are functions
+// Remember: a method is just a function with a receiver argument
+// Here's `Abs` written as a regular function with no change in functionality
+
+// ** 일반 함수 Abs
+func AbsFunc(v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func Practice4_2() {
+	v := Vertex{3, 4}
+	fmt.Println(AbsFunc(v))
+}
+
+// You can declare a method on non-struct types, too.
+// In this example we see a numeric type MyFloat with an Abs method.
+
+type MyFloat float64
+
+// You can only declare a method with a receiver whose type is defined
+// in the same package as the method.
+// You cannot declare a method with a receiver whose type is defined
+// in another package (int 같은 built-in types도 안 됨!!).
+
+func (f MyFloat) Abs4_3() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+func Practice4_3() {
+	f := MyFloat(-math.Sqrt2)
+	fmt.Println(f.Abs4_3())
+}
+
+// Pointer recievers
+
+// You can declare methods with pointer receivers.
+// This means the receiver type has the literal syntax *T for some type T.
+// (Also, T cannot itself be a pointer such as *int.)
+
+// For example, the Scale method here is defined on *Vertex.
+// Methods with pointer receivers can modify the value
+// to which the receiver points (as Scale does here).
+// Since methods often need to modify their receiver,
+// pointer receivers are more common than value receivers.
+
+// Try removing the * from the declaration of the Scale function
+// and observe how the program's behavior changes.
+// With a value receiver, the Scale method operates on a copy of the original 
+// Vertex value. (This is the same behavior as for any other function argument.)
+// The Scale method must have a pointer receiver to change the
+// Vertex value declared in the main function.
+
+func (v *Vertex) Scale(f float64) { // pointer receiver
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func (v Vertex) Scale2(f float64) { // value receiver
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func Practice4_4() {
+	v := Vertex{3, 4}
+	v2 := Vertex{3, 4}
+	v.Scale(10)
+	// 아래에서 v.Scale2(10)을 호출할 때 v2값의 복사본을 만든 후 넘김
+	// 따라서 Scale2 method에서 어떤 변화를 줘도 원본 v에는 영향이 없음
+	v2.Scale2(10)
+	fmt.Println(v.Abs())
+	fmt.Println(v2.Abs())
+}
+
+// Pointers and functions
+
+// Here we see the Abs and Scale methods rewritten as functions.
+// Again, try removing the * from line 16.
+// Can you see why the behavior changes?
+// What else did you need to change for the example to compile?
+// (If you're not sure, continue to the next page.)
+
+func ScaleFunc(v *Vertex, f float64) { // argument로 pointer를 받음
+	v.X *= f
+	v.Y *= f
+}
+
+func ScaleFunc2(v Vertex, f float64) {
+	v.X *= f
+	v.Y *= f
+}
+
+func Practice4_5() {
+	v := Vertex{3, 4}
+	// ScaleFunc(v, 10) // Complile Error
+	ScaleFunc2(v, 10)
+	fmt.Println(AbsFunc(v))
+	ScaleFunc(&v, 10)
+	fmt.Println(AbsFunc(v))
+}
+
+// Methods and pointer indirection
+// Practice4_5 보면 pointer argument를 받는 함수는 pointer 안 넣으면 컴파일 에러 발생
+// 하지만 pointer recievers 받는 메소드는 value든 pointer든 뭘 넘겨도 됨
+// pointer가 아니라 value를 넘겨도 pointer receiver가 자동으로 call 됨.
+
+func Practice4_6() {
+	
+}
+func Practice4_7() {
+	
+}
+func Practice4_8() {
+	
+}
+func Practice4_9() {
+	
+}
+func Practice4_10() {
+	
+}
+func Practice4_11() {
+	
+}
+func Practice4_12() {
+	
+}
+func Practice4_13() {
+	
+}
+func Practice4_14() {
+	
+}
+func Practice4_15() {
+	
+}
+func Practice4_16() {
+	
+}
+func Practice4_17() {
+	
+}
+func Practice4_18() {
+	
+}
+func Practice4_19() {
+	
+}
+func Practice4_20() {
+	
+}
+func Practice4_21() {
+	
+}
+func Practice4_22() {
+	
+}
+func Practice4_23() {
+	
+}
+func Practice4_24() {
+	
+}
+func Practice4_25() {
+	
+}
+func Practice4_26() {
+	
+}
